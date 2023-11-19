e16f32f5d004546522c3706bdfaabc87aef31142
carlos <carlos94_cr7@hotmail.com>
2023-11-18 23:35:34 -0600

    se agrego variable de entorno para la conexion a la db de mongo atlas

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,10 +2,11 @@ import { connect } from "mongoose";
 import "dotenv/config";
 
 const URL = <string>process.env.URL;
+const URL_MONGOATLAS = <string>process.env.URL_MONGOATLAS;
 
 async function createConnection(): Promise<void> {
   try {
-    const connectionDone = await connect(URL);
+    const connectionDone = await connect(URL_MONGOATLAS);
     if (connectionDone) console.log("CONNECTION TO MONGO DONE");
   } catch (error) {
     console.log(error);
